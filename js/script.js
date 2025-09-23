df76f0791413bb071af66f54e869574588b61ac6
Marissa Rico <marissa.rico28@gmail.com>
2025-09-23 11:10:57 +0200

    started js script: declared first function, grabbed elements, connected JS and HTML, prepared for state, helpers, collision, jump behaviour, and controls

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
-function startGame () {
-    const game = document.querySelector(".game");
+/*THE WRAPPER */
+function startGame() {
+  /*GRAB YOUR ELEMENTS- these connect your js with your html */
+  const game = document.querySelector(".game");
+  const player = document.getElementById("player");
+  const obstacle = document.getElementById("obstacle");
+  const goal = document.getElementById("goal");
 }
+
+/*HELPERS AND CONFIG*/
+
+/*STATE These are variables that change during the game:*/
+let isJumping = false;
+let score = 0;
+
+/*JUMP BEHAVIOUR*/
+function jump() {
+  if (isJumping) return;
+  isJumping = true;
+  
+}
+
+/*MOVE OBSTACLES*/
+
+/*COLLISION DETECTION   // Hit obstacle = lose*/
+
+/*controls key boards spacebar*/
+
+/*rezie adjustments*/
+
+/*end dame*/
+
+/*start it all with a loop -infitnite GAME LOOP/   // Reach goal = win*/
+
+/*start the game the wrapper*/
+startGame();
