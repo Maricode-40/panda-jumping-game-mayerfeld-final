33000d3bf760358158fc432221864c7434a3b600
Shalom Alalade <alaladeshalom02@gmail.com>
2025-09-23 16:59:52 +0100

    added highorder functions to enable  panda jump behaviour, and rAF for smooth sync framing animation, added event listners for mobile touch and desktops, added jump height and speed variables for.

--- js/script.js
+++ js/script.js
@@ -8,23 +8,68 @@ function startGame() {
 }
 
 /*HELPERS AND CONFIG*/
+const JUMP_HEIGHT = 120;
+const JUMP_SPEED = 5;
+
 
 /*STATE These are variables that change during the game:*/
 let isJumping = false;
 let score = 0;
+let gameRunning = true;
+
 
 /*JUMP BEHAVIOUR*/
 function jump() {
-  if (isJumping) return;
+  if (isJumping){ 
+    return;
+  }
   isJumping = true;
-  
+  let bottom = 0;
+  let goingUp = true;
+
+  function animateJump(){
+    if (!goingUp && bottom <= 0){
+      isJumping = false;
+      return;
+    }
+    if(goingUp && bottom >= JUMP_HEIGHT){
+      goingUp = false;
+      bottom += goingUp ? JUMP_SPEED : -JUMP_SPEED;
+      player.style.bottom = `${bottom}px`;
+      requestAnimationFrame(animateJump);
+       } 
+      requestAnimationFrame(animateJump);
+  }
 }
+
+
+
 
 /*MOVE OBSTACLES*/
 
 /*COLLISION DETECTION   // Hit obstacle = lose*/
 
 /*controls key boards spacebar*/
+/Keyboard (Spacebar)/
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    e.preventDefault();
+    if (!gameRunning) {
+      location.reload();
+    } else {
+      jump();
+    }
+  }
+});
+
+// Mouse click Touch tap
+game.addEventListener("click", () => {
+  if (!gameRunning) {
+    location.reload();
+  } else {
+    jump();
+  }
+});
 
 /*rezie adjustments*/
 
@@ -32,5 +77,14 @@ function jump() {
 
 /*start it all with a loop -infitnite GAME LOOP/   // Reach goal = win*/
 
+
+
+game.addEventListener("click", () => {
+  if (!gameRunning) {
+    location.reload();
+  } else {
+    jump();
+  }
+});
 /*start the game the wrapper*/
 startGame();
